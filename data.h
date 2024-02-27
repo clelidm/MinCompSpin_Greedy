1a2f5b94f628c59c6f841f152aada5a1a296aa0a
Clélia de Mulatier <clelidm@gmail.com>
2024-02-27 14:32:12 +0100

    Add files via upload

--- data.h
+++ data.h
@@ -3,22 +3,26 @@
 /********************************************************************/
 
 // number of binary (spin) variables:
-unsigned int n = 9;  
+unsigned int n = 121;  
 
 // INPUT FOLDER:
 const std::string input_directory = "INPUT/";
 
-// INPUT DATA FILES (optional):  
-// the input datafile can also be specified directly in the main() function, as an argument of the function "read_datafile()":
-std::string datafilename = "SCOTUS_n9_N895_Data.dat";
+// OUTPUT FOLDER:
+const std::string OUTPUT_directory = "OUTPUT/";
 
-// INPUT BASIS FILES (optional):
-std::string basis_filename = "SCOTUS_n9_BestBasis_Binary.dat";  // must be in binary representation
+
+// INPUT DATA FILES (optional):  // must be in binary representation: see Readme file
+std::string datafilename = "MNIST11.sorted"; // "SCOTUS_n9_N895_Data.dat";
+
+// INPUT BASIS FILES (optional):  // must be in binary representation: see Readme file
+std::string basis_filename = "MNIST11.sorted_BestBasis_k4_Binary.dat";  //"SCOTUS_n9_BestBasis_Binary.dat";
+
+
+// OTHER INPUT BASIS FILES (optional):
 const std::string basis_IntegerRepresentation_filename = "SCOTUS_n9_BestBasis_Integer.dat";        // (optional) Input basis file 
 const std::string basis_BinaryRepresentation_filename = "SCOTUS_n9_BestBasis_Binary.dat";      // (optional) Input basis file
 
 // INPUT MCM FILES (optional): // For example: choice of a community to compare with the uncovered community:
 const std::string communityfile = "INPUT/SCOTUS_Communities_inBestBasis.dat"; 
 
-// OUTPUT FOLDER:
-const std::string OUTPUT_directory = "OUTPUT/";
