4f36e7d2c546b837d367fe45958d4b0305b171e9
Clélia de Mulatier <clelidm@gmail.com>
2024-02-27 15:49:02 +0100

    Clean-up compilation

--- job_run.sh
+++ job_run.sh
@@ -1,6 +1,15 @@
 
-## RUN:
+### DATA:
 
-datafilename=INPUT/Big5-IPC1_VS3_N5e4.dat
-n=50
-time ./MCM_Greedy.out $datafilename $n
+datafilename=MNIST11.sorted   #Big5PT.sorted
+n=121  #50
+basisfilename=MNIST11.sorted_BestBasis_k4_Binary.dat  #Big5PT.sorted_BestBasis_Binary.dat 
+
+
+### RUN in ORIGINAL BASIS:
+
+time ./MCM_Greedy.out $datafilename $n 
+
+### RUN in chosen NEW BASIS:
+
+#time ./MCM_Greedy.out $datafilename $n -b MNIST11.sorted_BestBasis_k4_Binary.dat
